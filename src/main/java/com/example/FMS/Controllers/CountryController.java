e585c277c68d20f97ee8769a639e1c603fbbf827
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-12-30 15:26:32 +0530

    View & controllers updated!

--- src/main/java/com/example/FMS/Controllers/CountryController.java
+++ src/main/java/com/example/FMS/Controllers/CountryController.java
@@ -1,7 +1,13 @@
 package com.example.FMS.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CountryController {
+
+    @GetMapping("/Countries")
+    public String getCountries(){
+        return "country";
+    }
 }
