da45c2aea727c8397c17c513cc639025d8554bf4
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2021-12-30 15:52:04 +0530

    controllers updated!

--- src/main/java/com/example/FMS/Controllers/ClientController.java
+++ src/main/java/com/example/FMS/Controllers/ClientController.java
@@ -1,8 +1,14 @@
 package com.example.FMS.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
+
+    @GetMapping("/Clients")
+    public String getClient(){
+        return "Client";
+    }
 
 }
