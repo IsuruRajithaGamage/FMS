47400c00e6d9c09e89234f35395dadb9f0156b2b
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2022-01-01 01:27:41 +0530

    Page details updated!

--- src/main/java/com/example/FMS/Controllers/VehicleController.java
+++ src/main/java/com/example/FMS/Controllers/VehicleController.java
@@ -1,8 +1,14 @@
 package com.example.FMS.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleController {
+
+    @GetMapping("/vehicle")
+    public String getVehicle(){
+        return "Vehicle";
+    }
 
 }
