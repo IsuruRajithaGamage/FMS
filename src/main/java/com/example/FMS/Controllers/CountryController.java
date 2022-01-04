310e2edea091bfda83a41326f730cf4481b86097
Isuru Rajitha <isuru.rajitha.gamage@gmail.com>
2022-01-04 14:40:51 +0530

    Page tables updated!

--- src/main/java/com/example/FMS/Controllers/CountryController.java
+++ src/main/java/com/example/FMS/Controllers/CountryController.java
@@ -1,13 +1,26 @@
 package com.example.FMS.Controllers;
 
+import com.example.FMS.models.Country;
+import com.example.FMS.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
 
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/Countries")
-    public String getCountries(){
+    public String getCountries(Model model){
+        List<Country> countryList = countryService.getCountries();
+    model.addAttribute("countries", countryList);
         return "country";
     }
+
+
 }
--- src/main/java/com/example/FMS/services/CountryService.java
+++ src/main/java/com/example/FMS/services/CountryService.java
@@ -1,6 +1,18 @@
 package com.example.FMS.services;
+import com.example.FMS.models.Country;
+import com.example.FMS.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
 }
